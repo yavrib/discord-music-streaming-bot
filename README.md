69f0a58df60b6cf3eeb9fd6b91d927a8c5b26a46
Berkan Yavri <berkan.yavri@deliveryhero.com>
2019-10-25 18:58:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# discord-music-streaming-bot
+
+At some point in the future this is going to be a bot that you can clone and serve for yourself
---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = Discord.Client();
+
+client.on('ready', () => {
+
+});
+
+client.on('message', () => {
+
+});
+
+client.login('some-token');
